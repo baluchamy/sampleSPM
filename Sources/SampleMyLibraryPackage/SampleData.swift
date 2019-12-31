87eb37ff2c32c4ea05e33ff9396a67637e2387e1
baluchamy <baluchamy.muniyandi@impigertech.com>
2019-12-31 17:39:29 +0530

    sample added

---
+++ Sources/SampleMyLibraryPackage/SampleData.swift
@@ -0,0 +1,13 @@
+//
+//  File.swift
+//  
+//
+//  Created by Baluchamy  on 31/12/19.
+//
+
+import Foundation
+class SampleData {
+    var name: String?
+    var address: String?
+    var age: Int?
+}
