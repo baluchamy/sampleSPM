ecace7ec61cc1e818f21dd4adb62427763e7364f
baluchamy <baluchamy.muniyandi@impigertech.com>
2020-01-01 16:25:17 +0530

    custom initialisers added

--- Sources/SampleMyLibraryPackage/SampleData.swift
+++ Sources/SampleMyLibraryPackage/SampleData.swift
@@ -7,10 +7,24 @@
 
 import Foundation
 public class SampleData {
-    public init {
+    public init() {
         
     }
+    
+    public init(name: String, address: String) {
+        self.name = name
+        self.address = address
+     }
    public var name: String?
    public var address: String?
    public var age: Int?
+    
+    public printInfo() {
+        print("name:" ,name ?? "")
+        print("Address:", address ?? "")
+    }
+    
+    open func test() {
+        print("Hi")
+    }
 }
